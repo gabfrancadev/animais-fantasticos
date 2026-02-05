117f83d45b41ac02e73e6e835690d802924f898f
Gabriel França <gabfrancadev@gmail.com>
2026-02-05 19:22:38 -0300

    fix: mobile tooltip and dropdown menu

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -6,7 +6,8 @@ export default function initMenuMobile() {
   const eventos = ["click", "touchstart"];
 
   if (menuButton) {
-    function openMenu() {
+    function openMenu(event) {
+      event.preventDefault();
       menuList.classList.add("active");
       menuButton.classList.add("active");
       outsideClick(menuList, eventos, () => {
